f6f9015fd853cc49412a24b450dc7d7f8f66c878
Eric HOINVILLE <eric.hoinville@res-group.com>
2020-09-29 11:12:53 +0200

    First round commit

--- kata.py
+++ kata.py
@@ -1,2 +1,29 @@
-def a_function():
-                return 1
+def add_score(server_current_score, receiver_current_score, point_winner):
+    # point_winner = 0 --> server wins the point
+    # point_winner = 1 --> receiver wins the point
+    if point_winner == 1:
+        if receiver_current_score == 0:
+            receiver_current_score = receiver_current_score + 15
+        elif receiver_current_score == 15:
+            receiver_current_score = receiver_current_score + 15
+        elif receiver_current_score == 30:
+            receiver_current_score = receiver_current_score + 10
+        elif receiver_current_score == 40:    
+            if receiver_current_score == server_current_score:
+                receiver_current_score = A
+            else:
+                receiver_current_score = Win
+    else:
+         if server_current_score == 0:
+            server_current_score = server_current_score + 15
+         elif server_current_score == 15:
+            server_current_score = server_current_score + 15
+         elif server_current_score == 30:
+            server_current_score = server_current_score + 10
+         elif server_current_score == 40:    
+            if server_current_score == receiver_current_score:
+                server_current_score = A
+            else:
+                server_current_score = Win
+        
+    return server_current_score,receiver_current_score
--- test_kata.py
+++ test_kata.py
@@ -1,2 +1,5 @@
-def test_a_function():
-                assert a_function() == 1
+import kata
+
+server_current_score,receiver_current_score = kata.add_score(40,40,0)
+
+print("Score is: " + str(server_current_score) + ":" + str(receiver_current_score))
