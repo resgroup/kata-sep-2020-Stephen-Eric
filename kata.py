9737589427d7db1b3cecffb7712bc95dcbd16085
Eric HOINVILLE <eric.hoinville@res-group.com>
2020-09-29 12:55:16 +0200

    Third round

--- kata.py
+++ kata.py
@@ -1,34 +1,45 @@
-def add_score(server_current_score, receiver_current_score, point_winner):
-    # point_winner = 0 --> server wins the point
-    # point_winner = 1 --> receiver wins the point
-    if point_winner == 1:
-        if receiver_current_score == 0:
-            receiver_current_score = receiver_current_score + 15
-        elif receiver_current_score == 15:
-            receiver_current_score = receiver_current_score + 15
-        elif receiver_current_score == 30:
-            receiver_current_score = receiver_current_score + 10
-        elif receiver_current_score == 40:    
-            if receiver_current_score == server_current_score:
-                receiver_current_score = "A"
-            else:
-                receiver_current_score = "Win"
-        elif receiver_current_score == "A" and point_winner==1:
-            receiver_current_score="Win"
-        else:
-            receiver_current_score=40
+class TennisGame:
 
-    else:
-         if server_current_score == 0:
-            server_current_score = server_current_score + 15
-         elif server_current_score == 15:
-            server_current_score = server_current_score + 15
-         elif server_current_score == 30:
-            server_current_score = server_current_score + 10
-         elif server_current_score == 40:    
-            if server_current_score == receiver_current_score:
-                server_current_score = "A"
+    def __init__(self,server_current_score,receiver_current_score):
+        self.server_current_score = 0
+        self.receiver_current_score = 0
+
+    def add_score(self,server_current_score,receiver_current_score, point_winner):
+        # point_winner = 0 --> server wins the point
+        # point_winner = 1 --> receiver wins the point
+        if point_winner == 1:
+            if self.receiver_current_score == 0:
+                self.receiver_current_score = self.receiver_current_score + 15
+            elif self.receiver_current_score == 15:
+                self.receiver_current_score = self.receiver_current_score + 15
+            elif self.receiver_current_score == 30:
+                self.receiver_current_score = self.receiver_current_score + 10
+            elif self.receiver_current_score == 40 and self.server_current_score != "A":
+                if self.receiver_current_score == self.server_current_score:
+                    self.receiver_current_score = "A"
+                else:
+                    self.receiver_current_score = "Win"
+            elif self.server_current_score == "A":
+                self.server_current_score=40
             else:
-                server_current_score = "Win"
-        
-    return server_current_score,receiver_current_score
+                self.receiver_current_score="Win"
+
+        else:
+            if self.server_current_score == 0:
+                self.server_current_score = self.server_current_score + 15
+            elif self.server_current_score == 15:
+                self.server_current_score = self.server_current_score + 15
+            elif self.server_current_score == 30:
+                self.server_current_score = self.server_current_score + 10
+            elif self.server_current_score == 40 and self.receiver_current_score != "A":    
+                if self.server_current_score == self.receiver_current_score:
+                    self.server_current_score = "A"
+                else:
+                    self.server_current_score = "Win"
+            elif self.receiver_current_score == "A":
+                self.receiver_current_score=40
+            else:
+                self.server_current_score="Win"
+            
+            
+        return self.server_current_score,self.receiver_current_score
